267010024bec3b580d979a0ffcf01a3cb58ae9c7
Nasef <iamnasef@gmail.com>
2025-08-07 14:17:54 +0300

    Apple

--- memorize/memorize/ContentView.swift
+++ memorize/memorize/ContentView.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 struct ContentView: View{
     var body: some View {
-        VStack{
+        VStack (alignment: .trailing, spacing: 100){
             Text("Hello, World").foregroundColor(.black)
             Test()
+            imageApple()
             Image(systemName: "globe").foregroundColor(.green)
         }.padding()
         
@@ -21,6 +22,12 @@ struct ContentView: View{
 struct Test:View {
     var body: some View {
         Text("Nasef").foregroundColor(.orange).padding()
+    }
+}
+
+struct imageApple : View {
+    var body: some View {
+        Image("apple").foregroundColor(.brown).padding()
     }
 }
 
