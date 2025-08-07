18c7e4b362172b81238bad131effc865b386ca98
Nasef <iamnasef@gmail.com>
2025-08-07 11:09:16 +0300

    creating another struct text, and added to main struct

---
+++ memorize/memorize/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by cybname cyblname on 07/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View{
+    var body: some View {
+        VStack{
+            Text("Hello, World").foregroundColor(.black)
+            Test()
+        }.padding()
+        
+    }
+}
+
+struct Test:View {
+    var body: some View {
+        Text("Nasef").foregroundColor(.orange).padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ memorize/memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by cybname cyblname on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
