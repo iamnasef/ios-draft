979dd1bb0be98e593dab9e80efb6120fdbbae559
Nasef <iamnasef@gmail.com>
2025-08-17 12:35:23 +0300

    Dictionaries

--- data.playground/Contents.swift
+++ data.playground/Contents.swift
@@ -10,4 +10,14 @@ print("I'm \(name) and I'm \(age)")
 
 var color = ["red","yellow"]
 color.append("blue")
+
+color.remove(at: 0)
 print(color)
+color.contains("red")
+
+let person = [
+"name":"nasef",
+"age":"25",
+"job":"tech"
+]
+print(person["name",default: "Unknown"])
