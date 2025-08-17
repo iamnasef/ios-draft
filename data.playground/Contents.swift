eb8dd6d9c6c4ed726c8ee661c9eb7dbff6ee8a5d
Nasef <iamnasef@gmail.com>
2025-08-17 12:27:49 +0300

    appending to array

--- data.playground/Contents.swift
+++ data.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 print(Int.random(in: 1...30))
+print(1<20)
+
+let name = "nasef"
+let age = 24
+
+print("I'm \(name) and I'm \(age)")
+
+var color = ["red","yellow"]
+color.append("blue")
+print(color)
